8ddf9dec3d7eb30db713db07300c6f4c121210c4
annabalves <annaalves197@gmail.com>
2023-04-13 21:00:10 -0300

    aaaa:fire:

---
+++ pages/filmes/index.js
@@ -0,0 +1,43 @@
+import React ,{ useEffect, useState }  from 'react'
+import Pagina from '../../components/pagina'
+import apiFilmes from '../../services/apiFilmes'
+import { Button, Card, Col, Row } from 'react-bootstrap'
+
+const index = () => {
+
+  const [filmes, setFilmes] = useState([])
+
+  useEffect(()=>{
+
+    apiFilmes.get('/movie/popular').then(resultado=>{
+      setFilmes(resultado.data.results)
+    })
+
+  }, [])
+
+  return (
+    <Pagina titulo="Filmes">
+
+      <Row md={4}>
+
+      {filmes.map(item => (
+        <Col>
+        <Card className='mb-3'>
+          <Card.Img variant='top' src={'https://image.tmdb.org/t/p/w500'+item.backdrop_path}></Card.Img>
+          <Card.Body>
+            <Card.Title>{item.title}</Card.Title>
+            <Card.Text>Lançamento: {item.release_date}</Card.Text>
+            <Card.Text>Nota: {item.vote_average}</Card.Text>
+            <Button variant='primary'>Detalhes</Button>
+          </Card.Body>
+        </Card>
+        </Col>
+      ))}
+      </Row>
+
+
+    </Pagina>
+  )
+}
+
+export default index
